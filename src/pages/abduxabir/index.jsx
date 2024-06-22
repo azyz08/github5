837f7d82c7d99b4f6bbb7c39db71b72e67a87440
Abduxabir2024 <abduqayumovabduxabir2@gmail.com>
2024-06-22 15:10:20 +0500

    page ikki

--- src/pages/abduxabir/index.jsx
+++ src/pages/abduxabir/index.jsx
@@ -2,7 +2,41 @@ import "./stil.scss"
 export default function Abduxabir() {
     return (
         <>
-            <h1>Page2</h1>
+          <div className="bosh">
+            <h1>The Quickest Way To Onboard Talent To Your Team</h1>
+            <p>Growmodo is a design & development subscription that takes the pain out of hiring creatives for your business.</p>
+          </div>
+          <div className="bosh1">
+            <div className="card">
+                <h1>Subscribe</h1>
+                <p>to a plan & request your first project or task.</p>
+                <img src="./pop.png" alt="" />
+            </div>
+            <div className="card">
+                <h1>Subscribe</h1>
+                <p>to a plan & request your first project or task.</p>
+                <img src="./pop.png" alt="" />
+            </div> 
+            <div className="card">
+                <h1>Subscribe</h1>
+                <p>to a plan & request your first project or task.</p>
+                <img src="./pop.png" alt="" />
+            </div>
+          </div>
+          <div className="card1">
+            <div className="card2">
+                <img src="./io.png" alt="" />
+            </div>
+            <div className="card3">
+                <p>"I have been working with Growmodo for nearly two years now. With their keen eye for design, development skills, and expert project management, I have managed to serve a lot more clients than I would without them."</p>
+                <div className="card4">
+                  <br />
+                    <span>Gaute Remen</span>
+                    <br />
+                    <span>Tech Lead, Synlighet</span>
+                </div>
+            </div>
+          </div>
         </>
     )
 }
