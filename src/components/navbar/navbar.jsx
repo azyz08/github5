78f3c479cd070b5a0047a4fb43e685199395d23a
azyz08 <ibroximova014@gmail.com>
2024-06-22 13:04:24 +0500

    1page tugadi

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,15 +1,27 @@
 import { Link, Route, Routes } from "react-router-dom";
 import Azizbek from "../../pages/azizbek";
 import Abduxabir from "../../pages/abduxabir";
+import "./styles.scss"
 
 export default function Navbar() {
     return (
         <>
             <div className="nav">
-                <Link to={"/"}>Azizbek</Link>
-                <Link to={"/page2"}>Abduxabir</Link>
+                <h6>.</h6>
+                <input type="checkbox" id="m" />
+                <ul>
+                    <div className="labelBox">
+                        <label htmlFor="m"><i class="fa-solid fa-xmark"></i></label>
+                        <p>.</p>
+                    </div>
+                    <Link to={"/"}><p>Azizbek</p></Link>
+                    <Link to={"/page2"}><p>Abduxabir</p></Link>
+                    <Link to={"/"}><p>Pricing</p></Link>
+                    <Link to={"/"}><p>Resources</p></Link>
+                </ul>
+                <label htmlFor="m"><i class="fa-solid fa-bars"></i></label>
+                <button>Book a Call</button>
             </div>
-
 
             <Routes>
                 <Route path="/" element={<Azizbek />} />
--- src/pages/azizbek/index.jsx
+++ src/pages/azizbek/index.jsx
@@ -2,7 +2,59 @@ import "./stil.scss"
 export default function Azizbek() {
     return (
         <>
-            <h1>Page1</h1>
+            <div className="text">
+                <h1>Hire Your Whole Design & <br /> Dev Team With a Few Clicks</h1>
+                <p>Get on-demand access to your own team of designers, developers & <br /> project managers without the hassle of managing full-time employees.</p>
+            </div>
+
+            <div className="textBox">
+                <div className="top">
+                    <h1>We’ll customize your experience</h1>
+                    <h3>See Previous Work</h3>
+                </div>
+                <p>We use cookies and other technologies to keep our sites reliable and secure, to measure their performance, to deliver</p>
+                <p>a personalized experience and personalized advertising.</p>
+                <p>To do this, we collect information about users, their behaviour, and their devices.</p>
+                <p>”, you accept this and agree that we may share this information with third parties, such as our marketing partners. If</p>
+            </div>
+
+            <div className="off">
+                <p>Want the Secret Sauce of High-Performing Websites? Get 150+ Editable Website Cheatsheets</p>
+                <button>GET 50% OFF NOW ➞</button>
+            </div>
+
+            <div className="users">
+                <div className="user user1">
+                    <div className="desc desc1">
+                        <h3>Joshua B.</h3>
+                        <p>UI/UX Designer</p>
+                    </div>
+                </div>
+                <div className="user user2">
+                    <div className="desc desc2">
+                        <h3>Joshua B.</h3>
+                        <p>UI/UX Designer</p>
+                    </div>
+                </div>
+                <div className="user user3">
+                    <div className="desc desc3">
+                        <h3>Joshua B.</h3>
+                        <p>UI/UX Designer</p>
+                    </div>
+                </div>
+                <div className="user user4">
+                    <div className="desc desc4">
+                        <h3>Joshua B.</h3>
+                        <p>UI/UX Designer</p>
+                    </div>
+                </div>
+                <div className="user user5">
+                    <div className="desc desc5">
+                        <h3>Joshua B.</h3>
+                        <p>UI/UX Designer</p>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
